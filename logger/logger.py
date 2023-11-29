2fd1d72c88f32040d306d145818ee87eff92e703
BGKeogh <151687025+BGKeogh@users.noreply.github.com>
2023-11-28 19:18:48 -0600

    Update logger.py

--- logger/logger.py
+++ logger/logger.py
@@ -12,6 +12,16 @@ def logger_loop(adc_data, # ph and od sensors
     light_state = light_data['state'] # State of lights
     
     # TODO: Add logging code @Brennan
-    
-    time.sleep(5) # Set to whatever frequency we need
+    with  open('Data_Logging','a') as file: #appends to data log file, or creates if it doesnt exist
+      row = 0
+      if os.stat(filename).st_size == 0:
+        file.write("Time, pH, OD600, Light")
+      while True:
+        row += 1
+        now = datetime.now() #Gets current time. Time stamps
+        file.write(str(now) + "," + str(ph) + 'OD Measurement' + ',' + str(light_state)) #Writes time of measurement, and 
+        file.flush()
+        time.sleep(5) #Set to whatever frequency we need
+        file.close()
+      
     
