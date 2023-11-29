6474f3e373905b9d54e6057155c2e17e277ac01e
gtzook <gabyzook@gmail.com>
2023-11-29 09:48:34 -0600

    better debugging

--- main.py
+++ main.py
@@ -10,12 +10,15 @@ import sys
 
 if __name__ == "__main__":
     # Check cmd line args
-    debug_mode = False
+    adc_debug_mode = False
+    usb_debug_mode = False
     dark_mode = True
     if len(sys.argv) > 1:
         # pass in 'd' to enable debugging
-        if '-d' in sys.argv:
-            debug_mode = True
+        if '-a' in sys.argv:
+           adc_debug_mode = True
+        if '-u' in sys.argv:
+            usb_debug_mode = True
         if '-nolight' in sys.argv:
             dark_mode = True
     
@@ -35,12 +38,12 @@ if __name__ == "__main__":
     # ADC serial monitor
     usb_proc = mp.Process(name='usb', 
                         target=usb.adc.ADC_loop,
-                        args=[adc_data, new_ph_event, debug_mode]) 
+                        args=[adc_data, new_ph_event, adc_debug_mode]) 
     
     # Light controller
     light_proc =  mp.Process(name='lights', 
                             target=lights.light_controller.led_loop,
-                            args=[light_data, debug_mode])
+                            args=[light_data, usb_debug_mode])
     # GUI
     gui_proc = mp.Process(name = 'gui',
                           target=gui.gui.gui_loop,
