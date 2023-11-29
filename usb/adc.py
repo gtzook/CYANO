1c20f2e722e4f37120f07f66999ce1ea09b70361
gtzook <gabyzook@gmail.com>
2023-11-28 19:31:23 -0600

    adc adjustment /4

--- usb/adc.py
+++ usb/adc.py
@@ -17,7 +17,7 @@ class ADC():
     
     def get_ph(self):
         val = self.get()
-        voltage = 3.3 * (val / 255)
+        voltage = 3.3 * (val / 4096)
         # equation from https://files.atlas-scientific.com/Gravity-pH-datasheet.pdf
         return (-5.6548 * voltage) + 15.509
     
