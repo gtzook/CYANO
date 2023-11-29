c90c3efd099db23befa6bcfd31b8ad573e9185a7
gtzook <gabyzook@gmail.com>
2023-11-28 21:09:10 -0600

    model linear hoperfully

--- usb/adc.py
+++ usb/adc.py
@@ -20,7 +20,7 @@ class ADC():
         val = self.get()
         voltage = 3.3 * (val / 4096)
         # equation from https://files.atlas-scientific.com/Gravity-pH-datasheet.pdf
-        return (-5.6548 * voltage) + 15.509
+        return (-5.6548 * voltage) + 14.509
     
     def get(self):
         line=self.get_line()
