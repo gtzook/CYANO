8aa467d24d659a334f716a9e811c051478fe532e
gtzook <gabyzook@gmail.com>
2023-11-28 19:41:34 -0600

    debuggin

--- usb/adc.py
+++ usb/adc.py
@@ -18,14 +18,14 @@ class ADC():
     
     def get_ph(self):
         val = self.get()
+        if self.debug_mode:
+            print(f"adc val: {val}")
         voltage = 3.3 * (val / 1024)
         # equation from https://files.atlas-scientific.com/Gravity-pH-datasheet.pdf
         return (-5.6548 * voltage) + 15.509
     
     def get(self):
         line=self.get_line()
-        if self.debug_mode:
-            print(line)
         if self.num_sensors > 1:
             vals = list()
             for sense in range(self.num_sensors):
