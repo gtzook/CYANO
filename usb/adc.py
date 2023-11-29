8b39b2c1dab35ce118959c4ea2963c7dafaa3906
gtzook <gabyzook@gmail.com>
2023-11-28 20:47:16 -0600

    asdsa

--- usb/adc.py
+++ usb/adc.py
@@ -18,7 +18,7 @@ class ADC():
     
     def get_ph(self):
         val = self.get()
-        voltage = 3.3 * (val / 1024)
+        voltage = 3.3 * (val / 255)
         # equation from https://files.atlas-scientific.com/Gravity-pH-datasheet.pdf
         return (-5.6548 * voltage) + 15.509
     
