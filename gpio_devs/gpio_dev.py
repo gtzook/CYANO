4f21940078a25589e89f720241f6b4e17786197d
gtzook <gabyzook@gmail.com>
2023-12-04 13:09:36 -0600

    inheritance

--- gpio_devs/gpio_dev.py
+++ gpio_devs/gpio_dev.py
@@ -6,9 +6,12 @@ import pigpio
 pi = pigpio.pi()
 
 class gpio_dev:
+    dev_count = 0
     def __init__(self, pin: int, reverse_polarity: bool=False):
         self.pin = pin
         self.reverse_polarity = reverse_polarity
+        gpio_dev.dev_count += 1
+        print(f"gpio_dev initialized pin {self.pin}, dev_count: {gpio_dev.dev_count}")
         pi.set_mode(self.pin,pigpio.OUTPUT)
         self._set_handlers()
         self.off()
@@ -36,14 +39,12 @@ class gpio_dev:
         # handle closing
         print(f"gpio_dev pin {self.pin}: Exiting cleanly")
         self.off()
-        time.sleep(1)
-        pi.stop()
+        time.sleep(.1)
         sys.exit(0)
 
-class pwm_dev:
-    def __init__(self, pin: int, pwmFreq: int =25000, pwmDuty: int = 50):
-        self.pin = pin
-        self._set_handlers()
+class pwm_dev(gpio_dev):
+    def __init__(self, pin: int, pwmFreq: int = 25000, pwmDuty: int = 50):
+        super().__init__(pin)
         pi.set_PWM_frequency(self.pin,pwmFreq)
         self.duty = pwm_dev._percent_duty_convert(pwmDuty)
         
@@ -52,19 +53,6 @@ class pwm_dev:
     
     def off(self):
         pi.set_PWM_dutycycle(self.pin, 0)
-        
-    def _set_handlers(self):
-        # set handlers for exit to close cleanly
-        signal.signal(signal.SIGINT, self._exit)
-        signal.signal(signal.SIGTERM, self._exit)
-        
-    def _exit(self, signum, frame):
-        # handle closing
-        print(f"pwm_dev pin {self.pin}: Exiting cleanly")
-        self.off()
-        time.sleep(1)
-        pi.stop()
-        sys.exit(0)
     
     def _percent_duty_convert(val: int):
         return int((50/100.0) * 255)
