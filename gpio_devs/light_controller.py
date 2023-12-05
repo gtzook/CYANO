59bc36eb0d525b53ff253407625d557d9b464c42
gtzook <gabyzook@gmail.com>
2023-12-05 17:47:51 -0600

    timing

--- gpio_devs/light_controller.py
+++ gpio_devs/light_controller.py
@@ -42,11 +42,14 @@ def led_loop(shared_data: Dict[str, Union[int,float,bool]],
     signal.signal(signal.SIGTERM, cleanup)
     signal.signal(signal.SIGINT, cleanup)
     
+    # timing setup
     time_str = "%H:%M:%S" 
     night_time = dt.strptime(shared_data['to_night'], time_str)
     day_time = dt.strptime(shared_data['to_day'], time_str)
+    
     #toggle_time = shared_data['period']/2.0
     start_t = time.time()
+    toggle_time = 0
     
     while True:
         # Time elapsed since start of this state
