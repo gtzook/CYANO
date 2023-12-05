1f897b4c5d0f3a7a87cf5970cc215b9d406eecda
gtzook <gabyzook@gmail.com>
2023-12-04 20:04:26 -0600

    fix exiting behavior

--- gpio_devs/laser_controller.py
+++ gpio_devs/laser_controller.py
@@ -1,7 +1,8 @@
 from .gpio_dev import pwm_dev
 from typing import Dict, Union
 import multiprocessing as mp
-import time
+import signal
+import sys
 laser_pin = 13
 
 def laser_loop(shared_data: Dict[str, Union[int,float,bool]], 
@@ -11,8 +12,17 @@ def laser_loop(shared_data: Dict[str, Union[int,float,bool]],
     Main loop for laser
     
     Turn on laser
-    """
+    """    
     ctrl = pwm_dev(laser_pin)
+    
+    def cleanup(*args):
+        print("laser_controller: exiting cleanly")
+        ctrl.off()
+        sys.exit(0)
+    
+    signal.signal(signal.SIGTERM, cleanup)
+    signal.signal(signal.SIGINT, cleanup)
+    
     val = 100
     ctrl.set_duty_percent(val)
     if debug_mode:
