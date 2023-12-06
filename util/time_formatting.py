0833d3e62972ae56a1572d80cfef384dde89a2fa
gtzook <gabyzook@gmail.com>
2023-12-06 11:51:06 -0600

    time stuff

--- util/time_formatting.py
+++ util/time_formatting.py
@@ -44,10 +44,7 @@ def getTimeFromUser() -> dt.time:
 
 def is_time_between(begin_time: dt.time, end_time: dt.time, check_time=None):
     # If check time is not given, default to current time
-    check_time = check_time or dt.now().time()
-    print(f"begin_time: {begin_time}")
-    print(f"end_time: {end_time}")
-    print(f"now_time: {check_time}")
+    check_time = check_time or dt.now().time()\
     if begin_time < end_time:
         return check_time >= begin_time and check_time <= end_time
     else: # crosses midnight
