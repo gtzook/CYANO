60c0426cd4998516d91978f3333c56cd0368ec83
gtzook <gabyzook@gmail.com>
2024-01-12 15:11:30 -0600

    gui updates

--- gui/gui.py
+++ gui/gui.py
@@ -37,32 +37,49 @@ def gui_loop(shared_data: Dict[str, Union[int,float,bool]],
                 justification='center', font='Helvetica 20')],
               [sg.Canvas(size=(640, 60), key='-OD-CANVAS-')]]
     
+    #Value Display section
     status_column = [
     [sg.Text('pH Value:', font='Helvetica 32'), sg.Text('', key='-PH-VALUE-', font='Helvetica 32')],
     [sg.Text('', font='Helvetica 32'), sg.Text('', key="-BLANK-", font = 'Helvetica 32')],
-    [sg.Text('OD Value:', font='Helvetica 32'), sg.Text('', key='-OD-VALUE-', font='Helvetica 32')]
-]
+    [sg.Text('OD Value:', font='Helvetica 32'), sg.Text('', key='-OD-VALUE-', font='Helvetica 32')],
+    [sg.Button('Blank', font = "Helvetica 16", size = (10,3))]
+    ]
+
+    #Agiation Display section
+    agiation_column = [
+        [sg.Text('Agitation:', font = 'Helvetica 32'), sg.Text('', font = 'Helvetica 32', key='-AGITATION-PERCENT-')],
+        [sg.Button('Increase', font = 'Helvetica 16', size = (10,3)), sg.Button('Decrease', font= 'Helvetica 16', size = (10,3))],
+    ]
+
+    #Day Duration Slider
+    slider_column = [
+        [sg.Text("Daytime Hours:", font = "Helvetica 32")],
+        [sg.Slider(range=(0, 24), orientation="h", size=(60, 40), default_value=12, key="-SLIDER-")],
+        [sg.Button("OK"), sg.Button("Cancel")]
+    ]
+
     
     # light timer
     light_column = [
-            [sg.Text('DAY', size=(40, 3), key='-DAY-NIGHT-',
+            [sg.Text('DAY', size=(30, 3), key='-DAY-NIGHT-',
                 justification='center', font='Helvetica 40',
                 text_color='yellow')],
               
               [sg.Text('Time to switch: ', size=(40, 1),
                 justification='center', font='Helvetica 20',
                 text_color='gray'), # these elements will be next to each other
-              sg.Text('', size=(40, 1), key='-TIME-SWITCH-',
+              sg.Text('', size=(30, 1), key='-TIME-SWITCH-',
                 justification='center', font='Helvetica 20')]
             ]
     
     # define the form layout
+    #some of these are used for manipulating the spacing and location of the objects on the screen
     spacer_column = [sg.Column([[]], size=(140, 1))]
     s_c2 = [sg.Column([[]], size=(140, 1))]
     s_light = [sg.Column([[]], size=(325, 1))]
     top = [sg.Column([[]], size=(250, 200))]
-    layout = [[*top],
-            [*s_light, sg.Column(light_column, size = (3500, 350), element_justification = 'center'),],
+
+    layout = [[*top], [sg.Column(agiation_column), sg.Column(light_column, element_justification = 'center'), sg.Column(slider_column) ],
               [sg.Column(ph_column,vertical_alignment ='bottom'), *spacer_column, sg.Column(status_column), *s_c2, sg.Column(od_column, vertical_alignment = 'bottom')]
     ]
 
@@ -129,10 +146,17 @@ def gui_loop(shared_data: Dict[str, Union[int,float,bool]],
         od_line, =ax2.plot(range(od_datapoints), ods, 
                     color='green', linewidth=6)
 
+
+        agitation_percent = 0
+
         while True:
             event, _ = window.read(timeout=10)
             if event in ('Exit', None):
                 break
+            elif event == 'Increase':
+                agitation_percent  += 10
+            elif event == 'Decrease':
+                agitation_percent -= 10
                 
             phs.append(shared_data['ph'])  # add new ph data
             ph_line.set_ydata(phs)     # update plot
@@ -145,6 +169,8 @@ def gui_loop(shared_data: Dict[str, Union[int,float,bool]],
             
             window['-PH-VALUE-'].update(value="{:.3f}".format(shared_data['ph'])) # update text displays
             window['-OD-VALUE-'].update(value=f"{shared_data['od']}")
+            window['-AGITATION-PERCENT-'].update(f'{agitation_percent}%')
+            #window['-AGITATION-PERCENT'].update((value=)) #NEED TO SET AGITATION AMOUNT HERE
 
             if shared_data['state']:
                 window['-DAY-NIGHT-'].update(value='DAY',
