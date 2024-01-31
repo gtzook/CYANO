4018dab10ee1cfe981bb49d9ec64da5bdfd67b74
gtzook <gabyzook@gmail.com>
2024-01-31 14:01:02 -0600

    importing

--- test/ws2811.py
+++ test/ws2811.py
@@ -5,7 +5,7 @@ from gpio_devs.gpio_dev import gpio_dev
 import board
 import neopixel
 from time import sleep
-pixels = neopixel.NeoPixel(board.D19, 30)
+pixels = neopixel.NeoPixel(board.D18, 30)
 
 LED_pin = 26
 
