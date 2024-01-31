c200005118d471281174d983fe7d14a310788c0d
gtzook <gabyzook@gmail.com>
2024-01-31 14:34:34 -0600

    rainbow

--- test/ws2811.py
+++ test/ws2811.py
@@ -7,6 +7,8 @@ import neopixel
 import random
 from time import time
 from time import sleep
+import numpy as np
+import signal
 
 ORDER = neopixel.RGB
 
@@ -14,8 +16,15 @@ pixels = neopixel.NeoPixel(
     board.D18, 100, brightness=1, auto_write=True, pixel_order=ORDER
 )
 
-#pixels = neopixel.NeoPixel(board.D18, 3)
-
+def cleanup(*args):
+        print("light_controller: Exiting cleanly")
+        ctrl.off()
+        gpio_dev.cleanup()
+        sys.exit(0)
+    
+    signal.signal(signal.SIGTERM, cleanup)
+    signal.signal(signal.SIGINT, cleanup)
+    
 #RELAY
 LED_pin = 26
 ctrl = gpio_dev(LED_pin, reverse_polarity=False)
@@ -24,16 +33,12 @@ ctrl.on()
 def random_color():
     return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-start_time = time()
+rainbow = np.load('gpio_devs/light_patterns/interpolated_rainbow.npy')
+pattern_index = 0
+
 while True:
-    for i in range(len(pixels)):
-        pixels[i] = random_color()
+    for i in range(len(pixels)): 
+        pixels[i] = rainbow[pattern_index + i]
+    pattern_index = pattern_index + 1 if pattern_index < len(rainbow) - 1 else 0     
     pixels.show()
     sleep(0.1)  # Adjust the sleep duration to change the speed of color change
-
-
-
-pixels.show()
-sleep(5)
-
-ctrl.off()
