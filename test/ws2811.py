ed6fdfcfd2ddf0e52bc1e47c11e56fd25ee65b20
gtzook <gabyzook@gmail.com>
2024-01-31 13:37:01 -0600

    red

--- test/ws2811.py
+++ test/ws2811.py
@@ -1,6 +1,7 @@
 from gpio_devs.gpio_dev import gpio_dev
 import board
 import neopixel
+from time import sleep
 pixels = neopixel.NeoPixel(board.D18, 30)
 
 LED_pin = 26
@@ -8,5 +9,6 @@ LED_pin = 26
 # Relay
 ctrl = gpio_dev(LED_pin, reverse_polarity=False)
 ctrl.on()
-
+pixels[0] = (255,0,0)
+sleep(1000)
 ctrl.off()
