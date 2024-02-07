d13839c66f5eb46bc081beddb071dfece99038b4
gtzook <gabyzook@gmail.com>
2024-02-07 11:23:02 -0600

    splash test

--- gui/gui.py
+++ gui/gui.py
@@ -89,7 +89,7 @@ def gui_loop(shared_data: Dict[str, Union[int,float,bool]],
         DISPLAY_TIME_MILLISECONDS = 5000
         splash=sg.Window('',
                 [[sg.Image(SPLASH_IMAGE_FILE)]], no_titlebar=True, keep_on_top=True)
-        splash.read(timeout=DISPLAY_TIME_MILLISECONDS, close=True)
+        #splash.read(timeout=DISPLAY_TIME_MILLISECONDS, close=True)
         
         # create the form and show it without the plot
         window = sg.Window('CYANO GUI',
