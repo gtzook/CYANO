f689acaf6faa5842597b94869094e194532218b8
gabe <gtz1@rice.edu>
2024-02-20 21:05:40 -0600

    agi messages

--- gui/mainwindow.cpp
+++ gui/mainwindow.cpp
@@ -20,7 +20,7 @@
 QT_CHARTS_USE_NAMESPACE
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 {
-    
+
     qApp->setStyle(QStyleFactory::create("Fusion"));
 
     // Create the central widget
@@ -36,10 +36,12 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 
     // Agitation Display section
     QLabel *agitationLabel = new QLabel("Agitation:", centralWidget);
-    QLabel *agitationValue = new QLabel("100%", centralWidget);
+    agitationValue = new QLabel("100%", centralWidget);
     agi = 100;
     QPushButton *increaseButton = new QPushButton("Increase", centralWidget);
     QPushButton *decreaseButton = new QPushButton("Decrease", centralWidget);
+    connect(increaseButton, &QPushButton::pressed, this, &MainWindow::agitationIncrease);
+    connect(decreaseButton, &QPushButton::pressed, this, &MainWindow::agitationDecrease);
 
     // Day Duration Slider
     QLabel *brightnessLabel = new QLabel("Brightness:", centralWidget);
@@ -48,22 +50,56 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
     brightnessSlider->setValue(50);
     brightnessSlider->setFixedHeight(200); // Adjust the height of the slider box
     brightnessSlider->setStyleSheet(
-    "QSlider::groove:horizontal {"
-    "    background: qlineargradient(x1:0, y1:0, x2:1, y2:1,"
-    "        stop:0 #bbb, stop:1 #ddd);"
-    "    border: 1px solid #999;"
-    "    height: 200px;"
-    "    border-radius: 4px;"
-    "}"
-    "QSlider::handle:horizontal {"
-    "    background: qlineargradient(x1:0, y1:0, x2:1, y2:1,"
-    "        stop:0 #eee, stop:1 #ccc);"
-    "    border: 1px solid #777;"
-    "    width: 300px;"
-    "    margin: -2px 0;"
-    "    border-radius: 4px;"
-    "}"
-);
+        "QSlider::groove:horizontal {"
+        "    border: 1px solid #bbb;"
+        "    background: white;"
+        "    height: 10px;"
+        "    border-radius: 4px;"
+        "}"
+        "QSlider::sub-page:horizontal {"
+        "    background: qlineargradient(x1: 0, y1: 0,    x2: 0, y2: 1,"
+        "        stop: 0 #66e, stop: 1 #bbf);"
+        "    background: qlineargradient(x1: 0, y1: 0.2, x2: 1, y2: 1,"
+        "        stop: 0 #bbf, stop: 1 #55f);"
+        "    border: 1px solid #777;"
+        "    height: 10px;"
+        "    border-radius: 4px;"
+        "}"
+        "QSlider::add-page:horizontal {"
+        "    background: #fff;"
+        "    border: 1px solid #777;"
+        "    height: 10px;"
+        "    border-radius: 4px;"
+        "}"
+        "QSlider::handle:horizontal {"
+        "    background: qlineargradient(x1:0, y1:0, x2:1, y2:1,"
+        "        stop:0 #eee, stop:1 #ccc);"
+        "    border: 1px solid #777;"
+        "    width: 13px;"
+        "    margin-top: -2px;"
+        "    margin-bottom: -2px;"
+        "    border-radius: 4px;"
+        "}"
+        "QSlider::handle:horizontal:hover {"
+        "    background: qlineargradient(x1:0, y1:0, x2:1, y2:1,"
+        "        stop:0 #fff, stop:1 #ddd);"
+        "    border: 1px solid #444;"
+        "    border-radius: 4px;"
+        "}"
+        "QSlider::sub-page:horizontal:disabled {"
+        "    background: #bbb;"
+        "    border-color: #999;"
+        "}"
+        "QSlider::add-page:horizontal:disabled {"
+        "    background: #eee;"
+        "    border-color: #999;"
+        "}"
+        "QSlider::handle:horizontal:disabled {"
+        "    background: #eee;"
+        "    border: 1px solid #aaa;"
+        "    border-radius: 4px;"
+        "}");
+
     QPushButton *okButton = new QPushButton("OK", centralWidget);
     connect(okButton, &QPushButton::pressed, this, &MainWindow::brightnessClicked);
 
@@ -92,7 +128,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
     phpen.setWidth(7);
     phpen.setColor(Qt::red);
     pHSeries->setPen(phpen);
-    
+
     pHChart->createDefaultAxes();
     pHXAxis = qobject_cast<QValueAxis *>(pHChart->axes(Qt::Horizontal).at(0));
     QValueAxis *pHYAxis = qobject_cast<QValueAxis *>(pHChart->axes(Qt::Vertical).at(0));
@@ -100,11 +136,11 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
     pHYAxis->setRange(0, 14);
     pHXAxis->setTitleText("Time");
     pHYAxis->setTitleText("pH");
-    
+
     pHChartView->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Expanding);
     pHLayout->addWidget(phLabel);
     pHLayout->addWidget(pHChartView);
-    bottomLayout->addLayout(pHLayout,.2);
+    bottomLayout->addLayout(pHLayout, .2);
     // OD section
     QVBoxLayout *odLayout = new QVBoxLayout();
     QLabel *odLabel = new QLabel("Optical Density Sensor", centralWidget);
@@ -117,7 +153,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
     odpen.setWidth(7);
     odpen.setColor(Qt::green);
     odSeries->setPen(odpen);
-    
+
     odChart->createDefaultAxes();
     odXAxis = qobject_cast<QValueAxis *>(odChart->axes(Qt::Horizontal).at(0));
     QValueAxis *odYAxis = qobject_cast<QValueAxis *>(odChart->axes(Qt::Vertical).at(0));
@@ -128,7 +164,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
     odChartView->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Expanding);
     odLayout->addWidget(odLabel);
     odLayout->addWidget(odChartView);
-    bottomLayout->addLayout(odLayout,.2);
+    bottomLayout->addLayout(odLayout, .2);
 
     // Create client interface
     socket.connectToHost("127.0.0.1", 12345);
@@ -150,17 +186,17 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
     mainLayout->addWidget(timeSwitchLabel);
     mainLayout->addWidget(timeSwitchValue);
     mainLayout->addLayout(bottomLayout);
-
 }
 
 void MainWindow::updateGUI()
 {
-    while (socket.bytesAvailable() > 0) {
-        qDebug()<<"update gui";
+    while (socket.bytesAvailable() > 0)
+    {
         QByteArray rawData = socket.readAll();
         // Parse the JSON data
         std::map<QString, QJsonValue> data = parseJSON(rawData);
-        if (data["success"].toBool()){
+        if (data["success"].toBool())
+        {
             auto count = pHSeries->count(); // number of existing points
             auto farthest_x = pHSeries->at(count - 1).x();
 
@@ -169,15 +205,17 @@ void MainWindow::updateGUI()
             auto od = data["od"].toDouble();
 
             auto pos = 0.0;
-            if(count > 0){                   
-                pos = farthest_x + DATA_INTERVAL;  // one further than highest
+            if (count > 0)
+            {
+                pos = farthest_x + DATA_INTERVAL; // one further than highest
             }
             // Add to line series
-            pHSeries->append(pos,ph);
-            odSeries->append(pos,od);
+            pHSeries->append(pos, ph);
+            odSeries->append(pos, od);
 
             // Scroll graphs
-            if(farthest_x > SCROLL_THRESH){
+            if (farthest_x > SCROLL_THRESH)
+            {
                 odXAxis->setMax(odXAxis->max() + DATA_INTERVAL);
                 odXAxis->setMin(odXAxis->min() + DATA_INTERVAL);
                 odSeries->remove(0);
@@ -195,46 +233,59 @@ std::map<QString, QJsonValue> MainWindow::parseJSON(QByteArray raw)
     QJsonDocument doc = QJsonDocument::fromJson(raw);
     std::map<QString, QJsonValue> ret; // return map
     QJsonValue success = false;
-    if (!doc.isNull()) {
-        if (doc.isObject()) {
+    if (!doc.isNull())
+    {
+        if (doc.isObject())
+        {
             QJsonObject jsonObj = doc.object();
-            // Iterate through JSON          
-            foreach(QString key, jsonObj.keys()) {
+            // Iterate through JSON
+            foreach (QString key, jsonObj.keys())
+            {
                 QJsonValue value = jsonObj.value(key);
                 // Add JSON things to map
-                ret.insert({key,value});
+                ret.insert({key, value});
             }
             success = true;
-        } else {
+        }
+        else
+        {
             qWarning() << "JSON data is not an object.";
         }
-    } else {
+    }
+    else
+    {
         qWarning() << "Failed to parse JSON data.";
     }
     ret.insert({"success", success});
     return ret;
 }
 
-void MainWindow::agitationDecrease(){
-    if (agi > 0){
+void MainWindow::agitationDecrease()
+{
+    if (agi > 0)
+    {
         agi -= 10;
     }
-    qDebug() << "agi dec";    
+    agitationSend();
 }
-void MainWindow::agitationIncrease(){    
-    if (agi <100){
+void MainWindow::agitationIncrease()
+{
+    if (agi < 100)
+    {
         agi += 10;
     }
-    qDebug() << "agi inc";    
+    agitationSend();
 }
-void MainWindow::agitationSend(){
-    QString s= QString("%1\n").arg(agi);
-    agitationValue->setText();
-    QString s = QString("a%1\n").arg(agi);
+void MainWindow::agitationSend()
+{
+    QString s = QString("%1\%").arg(agi);
+    agitationValue->setText(s);
+    s = QString("a%1").arg(agi);
     socket.write(s.toUtf8());
 }
-void MainWindow::brightnessClicked(){
-    QString s = QString("b%1\n").arg(brightnessSlider->value());
+void MainWindow::brightnessClicked()
+{
+    QString s = QString("b%1").arg(brightnessSlider->value());
     socket.write(s.toUtf8());
 }
 
@@ -245,4 +296,3 @@ int main(int argc, char *argv[])
     w.showFullScreen();
     return app.exec();
 }
-
